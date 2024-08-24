c65545626c6ac12159f236a346484585f7e021f5
Annie Qiu <annieqiu.24@dartmouth.edu>
2024-08-24 23:03:32 +0800

    compress image

--- src/utils/photos.ts
+++ src/utils/photos.ts
@@ -17,21 +17,12 @@ export async function getAlbumImages(albumId: string) {
   return resolvedImages;
 }
 
-export async function getNumImages(albumId: string) {
-  // 1. List all album files from collections path
-  let images = import.meta.glob<{ default: ImageMetadata }>(
-    "/src/content/albums/**/*.{jpeg,jpg,png,JPG,PNG}"
+export async function getPlayImages() {
+  const images = import.meta.glob<{ default: ImageMetadata }>(
+    "/src/assets/play/**/*.{jpeg,jpg,png,JPG,PNG,gif}"
   );
-
-  // 2. Filter images by albumId
-  images = Object.fromEntries(
-    Object.entries(images).filter(([key]) => key.includes(albumId))
-  );
-
-  // 3. Images are promises, so we need to resolve the glob promises
   const resolvedImages = await Promise.all(
-    Object.values(images).map((image) => image().then((mod) => mod.default))
-  );
-
-  return resolvedImages.length;
+      Object.values(images).map((image) => image().then((file) => file.default))
+    );
+  return resolvedImages;
 }
